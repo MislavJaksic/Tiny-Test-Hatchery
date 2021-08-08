413307479664b3433ba7811e8f1a406e5831cdb2
Mislav Jaksic <jaksicmislav@gmail.com>
2021-08-08 07:27:14 +0200

    Added hatchery product to gitignore

--- tiny_test_hatchery/runner.py
+++ tiny_test_hatchery/runner.py
@@ -14,8 +14,8 @@ from tiny_test_hatchery import pytest_gen
 
 def main(args):
     """main() will be run if you run this script directly"""
-    test_file_path = "test_pytest_gen.py"
-    program_file_path = "E:\GreatRepository\Publishing\GitHubRepositories\EVE-Tools\eve_tools\chat\collection.py"
+    test_file_path = "test_eggs.py"
+    program_file_path = "D:/Repositories/rapdevpy/rapdevpy/api_file_cache.py"
     pytest_gen.generate_test_file_from_file(test_file_path, program_file_path)
 
 
